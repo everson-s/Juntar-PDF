f1b09886c21426bc588abebc455bdc8862b90fae
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2022-05-01 19:01:44 -0300

    Atualizando o programa

--- Jundar-PDF/Jundar-PDF/Form1.Designer.vb
+++ Jundar-PDF/Jundar-PDF/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Descartar substituições de formulário para limpar a lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,61 @@ Partial Class Form1
     'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
     'Pode ser modificado usando o Windows Form Designer.  
     'Não o modifique usando o editor de códigos.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.OFD = New System.Windows.Forms.OpenFileDialog()
+        Me.FBD = New System.Windows.Forms.FolderBrowserDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArquivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OFD
+        '
+        Me.OFD.FileName = "OpenFileDialog1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArquivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(816, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArquivoToolStripMenuItem
+        '
+        Me.ArquivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AbrirToolStripMenuItem})
+        Me.ArquivoToolStripMenuItem.Name = "ArquivoToolStripMenuItem"
+        Me.ArquivoToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ArquivoToolStripMenuItem.Text = "Arquivo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(816, 532)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Juntar PDF"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents OFD As OpenFileDialog
+    Friend WithEvents FBD As FolderBrowserDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArquivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
 End Class
--- Jundar-PDF/Jundar-PDF/Form1.vb
+++ Jundar-PDF/Jundar-PDF/Form1.vb
@@ -1,3 +1,44 @@
-﻿Public Class Form1
+﻿Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+
+Public Class Form1
+
+    Dim EnderecoDiretorio As String
+
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+
+
+#Region "Para abrir os arquivos"
+
+        'Título da pasta que que contém os arquivos em PDF
+        OFD.Title = "Selecione os arquivos em PDF"
+
+        'O filter irá filtrar somente arquivos PDF's encontrados na pasta
+        OFD.Filter = "Arquivos em PDF |*.pdf"
+
+        'O Multiselect, sendo habilitado para verdadeiro, irá possibilitar que vários PDF's sejam selecionados
+        OFD.Multiselect = True
+
+        'Será o nome apresentado no TextBox antes da seleção dos arquivos
+        OFD.FileName = "Arquivos em PDF"
+
+        'Abre a pasta de Documentos do usuário
+        OFD.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+
+#End Region
+
+        'Abre uma pasta para selecionar os arquivos em PDF
+        If OFD.ShowDialog = DialogResult.OK Then
+
+
+
+            EnderecoDiretorio = 
+
+        End If
+
+        MessageBox.Show("Endereço do Diretório: " + EnderecoDiretorio)
+
+    End Sub
 
 End Class
